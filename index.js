3065cb8e6bf0500a800e3a19cb5947540d04f3cc
Ecateln <clement.oge@proton.me>
2025-06-03 17:30:07 +0200

    incomplete socket impletemation (client issues)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
-
+import { createServer } from 'http';
+import { Server } from "socket.io";
 // import { askAgent } from './util/functions.js';
 // const msgs = [];
 // async function askAndPrint(prompt) {
@@ -13,6 +14,16 @@ import express from 'express';
 // process.exit();
 
 const app = express();
+const server = createServer();
+const socket_server = new Server(server);
+
+socket_server.on('connection', (socket) => {
+    console.log('A user connected');
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
 const port = 8001;
 
 app.get('/', (req, res) => {
@@ -21,9 +32,22 @@ app.get('/', (req, res) => {
         .end();
 });
 
-app.all(/(.*)/, (req, res) => {
-    res.redirect("https://youtu.be/dQw4w9WgXcQ");
+app.get('/test_socket', (req, res) => {
+    res.status(200)
+        .send(`
+<script src="https://cdn.socket.io/3.1.3/socket.io.min.js" integrity="sha384-cPwlPLvBTa3sKAgddT6krw0cJat7egBga3DJepJyrLl4Q9/5WLra3rrnMcyTyOnh" crossorigin="anonymous"></script>
+<script>
+const io = require("socket.io-client");
+const socket = io();
+</script>
+<h1>Socket.IO Test</h1>
+<p>Open your browser console to see the connection logs.</p>`)
+        .end();
 });
+
+// app.all(/(.*)/, (req, res) => {
+//     res.redirect("https://youtu.be/dQw4w9WgXcQ");
+// });
 
 // Start server
 app.listen(
